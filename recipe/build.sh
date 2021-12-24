3b023311bd190784e364527a803187e043174db6
Matthias Diener <mdiener@illinois.edu>
2021-12-25 00:06:57 +0100

    test

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,12 @@ export GOPATH=$SRC_DR
 export PATH=${GOPATH}/bin:$PATH
 
 # Build
-make bin/gh manpages
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+    make bin/gh manpages
+    make test
+else
+    make bin/gh
+fi
 
 # Install
 make install prefix=$PREFIX
