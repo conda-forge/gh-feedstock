b0610dc3109568e3169787037acf03e77c7b3971
Matthias Diener <mdiener@illinois.edu>
2021-12-25 23:49:10 +0100

    cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,9 @@
 
 set -ex
 
-export GOPATH=$SRC_DR
-export PATH=${GOPATH}/bin:$PATH
-
 if [[ $target_platform == "osx-arm64" ]]; then
-    # build gen-docs manually as it needs to be run to create the man-pages
+    # When cross-compiling, build gen-docs and manpages separately as
+    # they need to be executed on the build platform.
     export GOARCH=amd64
     make manpages
     export GOARCH=arm64
@@ -14,13 +12,11 @@ else
     make manpages
 fi
 
-# Build
+# Build bin/gh
 make bin/gh
 
-# Install
-file bin/gh
-make install prefix=$PREFIX
-# mkdir -p $PREFIX/bin
-# install -m755 bin/gh ${PREFIX}/bin
-# mkdir -p $PREFIX/share/man/man1
-# install -m644 ./share/man/man1/* $PREFIX/share/man/man1/
+# Install manually, since `make install` will recreate the manpages
+mkdir -p $PREFIX/bin
+install -m755 bin/gh ${PREFIX}/bin
+mkdir -p $PREFIX/share/man/man1
+install -m644 ./share/man/man1/* $PREFIX/share/man/man1/
