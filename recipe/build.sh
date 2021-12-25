6a6bc8ec01d58f7b4710ea978cf77748390192ae
Matthias Diener <mdiener@illinois.edu>
2021-12-25 11:08:29 +0100

    new

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,10 @@ set -ex
 
 export GOPATH=$SRC_DR
 export PATH=${GOPATH}/bin:$PATH
-export GOARCH=$target_goarch
+
+if [[ $target_platform == "osx-arm64" ]]; then
+    export GOARCH=arm64
+fi
 
 # Build
 make bin/gh manpages
