588f35056ac620e65a5543d9c8f81ee2298702fa
Matthias Diener <mdiener@illinois.edu>
2021-12-25 19:02:22 +0100

    build gen-docs manually

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,8 @@ export GOPATH=$SRC_DR
 export PATH=${GOPATH}/bin:$PATH
 
 if [[ $target_platform == "osx-arm64" ]]; then
+    (cd cmd/gen-docs; go build -v -o gen-docs .)
+    go run ./cmd/gen-docs --man-page --doc-path ./share/man/man1/
     export GOARCH=arm64
 fi
 
