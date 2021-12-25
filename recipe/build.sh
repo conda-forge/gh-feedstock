10f024c390078ae9f577f60b2100e23573a67cab
Matthias Diener <mdiener@illinois.edu>
2021-12-25 23:04:27 +0100

    fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,7 @@ export PATH=${GOPATH}/bin:$PATH
 if [[ $target_platform == "osx-arm64" ]]; then
     # build gen-docs manually as it needs to be run to create the man-pages
     (export GOARCH=amd64; cd cmd/gen-docs; go build -v -o gen-docs .)
-    file ./cmd/gen-docs
+    file ./cmd/gen-docs/gen-docs
     go run ./cmd/gen-docs --man-page --doc-path ./share/man/man1/
     export GOARCH=arm64
 fi
