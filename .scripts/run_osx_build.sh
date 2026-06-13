e47d8abd3aa814cc3b70d0c55b5ae06fdf8acaf2
Pavel Zwerschke <pavelzw@gmail.com>
2026-06-14 00:02:15 +0200

    MNT: Re-rendered with conda-smithy 2026.5.30.dev67+g41b60f5ca.d20260611 and conda-forge-pinning 2026.06.13.16.55.23
    Other tools:
    - conda-build 26.5.0
    - rattler-build 0.65.1
    - rattler-build-conda-compat 1.4.14

--- .scripts/run_osx_build.sh
+++ .scripts/run_osx_build.sh
@@ -84,7 +84,15 @@ if [[ -f LICENSE.txt ]]; then
 fi
 
 if [[ "${BUILD_WITH_CONDA_DEBUG:-0}" == 1 ]]; then
-    echo "rattler-build does not currently support debug mode"
+    export CONDA_BLD_PATH="${CONDA_BLD_PATH:-${FEEDSTOCK_ROOT:-$PWD}/build_artifacts}"
+    rattler-build debug setup \
+        --recipe ./recipe \
+        -m ./.ci_support/${CONFIG}.yaml \
+        --target-platform "${HOST_PLATFORM}" \
+        ${BUILD_OUTPUT_ID:+--output-name "${BUILD_OUTPUT_ID}"} \
+        ${EXTRA_CB_OPTIONS:-}
+
+    rattler-build debug shell
 else
 
     if [[ "${HOST_PLATFORM}" != "${BUILD_PLATFORM}" ]]; then
